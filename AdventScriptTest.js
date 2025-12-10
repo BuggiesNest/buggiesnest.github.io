be8e825d05fd785f8c992d06d3184513a6246456
VulcanCakes <54117197+VulcanCakes@users.noreply.github.com>
2025-12-10 11:52:39 -0700

    Add files via upload

---
+++ AdventScriptTest.js
@@ -0,0 +1,123 @@
+function Window1Function() {
+if (new Date().getDate() >= 1){
+{document.getElementById('buggiewindow1').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window1Function2(){
+	document.getElementById('buggiewindow1').style.visibility = "hidden"
+};
+
+function Window2Function() {
+if (new Date().getDate() >= 2){
+{document.getElementById('buggiewindow2').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window2Function2(){
+	document.getElementById('buggiewindow2').style.visibility = "hidden"
+};
+
+function Window3Function() {
+if (new Date().getDate() >= 3){
+{document.getElementById('buggiewindow3').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window3Function2(){
+	document.getElementById('buggiewindow3').style.visibility = "hidden"
+};
+
+function Window4Function() {
+if (new Date().getDate() >= 4){
+{document.getElementById('buggiewindow4').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window4Function2(){
+	document.getElementById('buggiewindow4').style.visibility = "hidden"
+};
+
+function Window5Function() {
+if (new Date().getDate() >= 5){
+{document.getElementById('buggiewindow5').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window5Function2(){
+	document.getElementById('buggiewindow5').style.visibility = "hidden"
+};
+
+function Window6Function() {
+if (new Date().getDate() >= 6){
+{document.getElementById('buggiewindow6').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window6Function2(){
+	document.getElementById('buggiewindow6').style.visibility = "hidden"
+};
+
+function Window7Function() {
+if (new Date().getDate() >= 7){
+{document.getElementById('buggiewindow7').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window7Function2(){
+	document.getElementById('buggiewindow7').style.visibility = "hidden"
+};
+
+function Window8Function() {
+if (new Date().getDate() > 8){
+{document.getElementById('buggiewindow8').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window8Function2(){
+	document.getElementById('buggiewindow8').style.visibility = "hidden"
+};
+
+function Window9Function() {
+if (new Date().getDate() >= 9){
+{document.getElementById('buggiewindow9').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window9Function2(){
+	document.getElementById('buggiewindow9').style.visibility = "hidden"
+};
+
+function Window10Function() {
+if (new Date().getDate() >= 10){
+{document.getElementById('buggiewindow10').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window10Function2(){
+	document.getElementById('buggiewindow10').style.visibility = "hidden"
+};
+
+function Window11Function() {
+if (new Date().getDate() >= 11){
+{document.getElementById('buggiewindow11').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window11Function2(){
+	document.getElementById('buggiewindow11').style.visibility = "hidden"
+};
+
+function Window12Function() {
+if (new Date().getDate() >= 12){
+{document.getElementById('buggiewindow12').style.visibility = "visible"
+}}
+else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
+}}};
+function Window12Function2(){
+	document.getElementById('buggiewindow12').style.visibility = "hidden"
+};
+
+function WindowErrorFunction2(){
+	document.getElementById('buggiewindowerror').style.visibility = "hidden"
+};
