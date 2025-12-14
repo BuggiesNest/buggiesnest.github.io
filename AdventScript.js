c2640f18077273eb41ad212286110136972620db
Pyre <89794440+Fuefos@users.noreply.github.com>
2025-12-14 02:46:23 +0000

    Update AdventScript.js

--- AdventScript.js
+++ AdventScript.js
@@ -121,3 +121,32 @@ function Window12Function2(){
 function WindowErrorFunction2(){
 	document.getElementById('buggiewindowerror').style.visibility = "hidden"
 };
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+	
+	// Next/previous controls
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+	
+	function showSlides(n) {
+	  let i;
+	  let slides = document.getElementsByClassName("mySlides");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	    slides[i].style.display = "none";
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	}
+
+
+
+
+
+
+
+
