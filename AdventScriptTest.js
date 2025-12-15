63125aebb4100fc5a33afde2208d1553ae534c11
Pyre <89794440+Fuefos@users.noreply.github.com>
2025-12-15 05:18:26 +0000

    Update AdventScriptTest.js
    testing new js function

--- AdventScriptTest.js
+++ AdventScriptTest.js
@@ -1,5 +1,5 @@
 function Window1Function() {
-if (new Date().getDate() >= 1){
+if (new Date().getDate() >= 14){
 {document.getElementById('buggiewindow1').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -9,7 +9,7 @@ function Window1Function2(){
 };
 
 function Window2Function() {
-if (new Date().getDate() >= 2){
+if (new Date().getDate() >= 15){
 {document.getElementById('buggiewindow2').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -19,7 +19,7 @@ function Window2Function2(){
 };
 
 function Window3Function() {
-if (new Date().getDate() >= 3){
+if (new Date().getDate() >= 16){
 {document.getElementById('buggiewindow3').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -29,7 +29,7 @@ function Window3Function2(){
 };
 
 function Window4Function() {
-if (new Date().getDate() >= 4){
+if (new Date().getDate() >= 17){
 {document.getElementById('buggiewindow4').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -39,7 +39,7 @@ function Window4Function2(){
 };
 
 function Window5Function() {
-if (new Date().getDate() >= 5){
+if (new Date().getDate() >= 18){
 {document.getElementById('buggiewindow5').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -49,7 +49,7 @@ function Window5Function2(){
 };
 
 function Window6Function() {
-if (new Date().getDate() >= 6){
+if (new Date().getDate() >= 19){
 {document.getElementById('buggiewindow6').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -59,7 +59,7 @@ function Window6Function2(){
 };
 
 function Window7Function() {
-if (new Date().getDate() >= 7){
+if (new Date().getDate() >= 20){
 {document.getElementById('buggiewindow7').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -69,7 +69,7 @@ function Window7Function2(){
 };
 
 function Window8Function() {
-if (new Date().getDate() > 8){
+if (new Date().getDate() > 21){
 {document.getElementById('buggiewindow8').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -79,7 +79,7 @@ function Window8Function2(){
 };
 
 function Window9Function() {
-if (new Date().getDate() >= 9){
+if (new Date().getDate() >= 22){
 {document.getElementById('buggiewindow9').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -89,7 +89,7 @@ function Window9Function2(){
 };
 
 function Window10Function() {
-if (new Date().getDate() >= 10){
+if (new Date().getDate() >= 23){
 {document.getElementById('buggiewindow10').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -99,7 +99,7 @@ function Window10Function2(){
 };
 
 function Window11Function() {
-if (new Date().getDate() >= 11){
+if (new Date().getDate() >= 24){
 {document.getElementById('buggiewindow11').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -109,7 +109,7 @@ function Window11Function2(){
 };
 
 function Window12Function() {
-if (new Date().getDate() >= 12){
+if (new Date().getDate() >= 25){
 {document.getElementById('buggiewindow12').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -121,3 +121,48 @@ function Window12Function2(){
 function WindowErrorFunction2(){
 	document.getElementById('buggiewindowerror').style.visibility = "hidden"
 };
+
+
+
+let slideIndex = [1,1];
+let slideId = ["slidesDay2", "slidesDay3", "slidesDay4", "slidesDay5", "slidesDay6", "slidesDay7", "slidesDay8", "slidesDay9", "slidesDay10", "slidesDay11", "slidesDay12"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+showSlides(1, 6);
+showSlides(1, 7);
+showSlides(1, 8);
+showSlides(1, 9);
+showSlides(1, 10);
+
+showSlides(slideIndex);
+	
+	// Next/previous controls
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+};
+
+
+function testLoad(){
+	document.getElementById('buggiewindowerror').style.visibility = "visible"
+}
+
+
+
+
