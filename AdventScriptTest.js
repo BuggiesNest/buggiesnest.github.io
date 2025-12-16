ba6e10c4ac2d094d53c986d0aa089652aa4169b1
Pyre <89794440+Fuefos@users.noreply.github.com>
2025-12-16 03:19:04 +0000

    Update AdventScriptTest.js
    site will now keep working after jan 1st

--- AdventScriptTest.js
+++ AdventScriptTest.js
@@ -1,36 +1,52 @@
+const dn = Date.now();
+const d = new Date();
+const ds = d.getTimezoneOffset()*60;
+const da = dn-ds;
+
+
+
 function Window1Function() {
-if (new Date().getDate() >= 14){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-14"))){
 {document.getElementById('buggiewindow1').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window1Function2(){
 	document.getElementById('buggiewindow1').style.visibility = "hidden";
     document.getElementById('bugvideo').pause();
 };
 
+
+
 function Window2Function() {
-if (new Date().getDate() >= 15){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-15"))){
 {document.getElementById('buggiewindow2').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window2Function2(){
 	document.getElementById('buggiewindow2').style.visibility = "hidden"
 };
 
+
+
 function Window3Function() {
-if (new Date().getDate() >= 16){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-16"))){
 {document.getElementById('buggiewindow3').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window3Function2(){
 	document.getElementById('buggiewindow3').style.visibility = "hidden"
 };
 
+
+
 function Window4Function() {
-if (new Date().getDate() >= 17){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-17"))){
 {document.getElementById('buggiewindow4').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
@@ -39,85 +55,111 @@ function Window4Function2(){
 	document.getElementById('buggiewindow4').style.visibility = "hidden"
 };
 
+
+
 function Window5Function() {
-if (new Date().getDate() >= 18){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-18"))){
 {document.getElementById('buggiewindow5').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window5Function2(){
 	document.getElementById('buggiewindow5').style.visibility = "hidden"
 };
 
+
+
 function Window6Function() {
-if (new Date().getDate() >= 19){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-19"))){
 {document.getElementById('buggiewindow6').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window6Function2(){
 	document.getElementById('buggiewindow6').style.visibility = "hidden"
 };
 
+
+
 function Window7Function() {
-if (new Date().getDate() >= 20){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-20"))){
 {document.getElementById('buggiewindow7').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window7Function2(){
 	document.getElementById('buggiewindow7').style.visibility = "hidden"
 };
 
+
+
 function Window8Function() {
-if (new Date().getDate() > 21){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-21"))){
 {document.getElementById('buggiewindow8').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window8Function2(){
 	document.getElementById('buggiewindow8').style.visibility = "hidden"
 };
 
+
+
 function Window9Function() {
-if (new Date().getDate() >= 22){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-22"))){
 {document.getElementById('buggiewindow9').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window9Function2(){
 	document.getElementById('buggiewindow9').style.visibility = "hidden"
 };
 
+
+
 function Window10Function() {
-if (new Date().getDate() >= 23){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-23"))){
 {document.getElementById('buggiewindow10').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window10Function2(){
 	document.getElementById('buggiewindow10').style.visibility = "hidden"
 };
 
+
+
 function Window11Function() {
-if (new Date().getDate() >= 24){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-24"))){
 {document.getElementById('buggiewindow11').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window11Function2(){
 	document.getElementById('buggiewindow11').style.visibility = "hidden"
 };
 
+
+
 function Window12Function() {
-if (new Date().getDate() >= 25){
+if ((Date.now()-(((new Date()).getTimezoneOffset()*60)*1000)) >= (new Date("2025-12-25"))){
 {document.getElementById('buggiewindow12').style.visibility = "visible"
 }}
 else{{document.getElementById('buggiewindowerror').style.visibility = "visible"
 }}};
+
 function Window12Function2(){
 	document.getElementById('buggiewindow12').style.visibility = "hidden"
 };
+
+
 
 function WindowErrorFunction2(){
 	document.getElementById('buggiewindowerror').style.visibility = "hidden"
@@ -161,7 +203,6 @@ function showSlides(n, no) {
 function testLoad(){
 	document.getElementById('buggiewindowerror').style.visibility = "visible"
 }
-
 
 
 
